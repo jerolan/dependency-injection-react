0e6b062283e7dbe9220fbc40dc39bfcbf5c100b6
Jerome Olvera <jerome.olvera@gmail.com>
2022-06-26 22:58:48 -0500

    style shopping cart counter

--- src/components/nav-bar.js
+++ src/components/nav-bar.js
@@ -6,8 +6,10 @@ export default function NavBar() {
   return (
     <header className="flex items-center justify-between h-16 border px-16">
       <h1 className="font-bold">super awesome</h1>
-      <div>
-        {items.length}
+      <div className="flex items-center space-x-3">
+        <p className="text-center px-2 font-bold text-white rounded-full bg-sky-400">
+          {`Items in cart ${items.length}`}
+        </p>
         <figure>
           <div className="rounded-full bg-indigo-600 h-8 w-8" />
         </figure>
